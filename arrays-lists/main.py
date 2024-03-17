23bf91efec217f05079987e85acffc1c42ab2b7f
Bob Put <bobu.work@gmail.com>
2024-03-17 19:25:56 -0400

    Add common_elements function to find common elements in two lists

--- arrays-lists/main.py
+++ arrays-lists/main.py
@@ -10,5 +10,19 @@ def remove_duplicates(input_list):
             output_list.append(list_element)
     print(output_list)
 
+'''
+Find Common Elements in Two Lists
+'''
+def common_elements(list1,list2):
+    common = []
+    largest = list1 if len(list1) > len(list2) else list2
+    for element in largest:
+        if element in list1 and element in list2:
+            common.append(element)
+    print(common)
 
-remove_duplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+
+# remove_duplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+common_elements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [ 6, 7, 16, 17, 18, 19, 20])
