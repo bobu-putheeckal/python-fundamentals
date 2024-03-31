a22117f048a1befca5d941955a5cb3297b4193d8
Bob Put <bobu.work@gmail.com>
2024-03-31 09:57:06 -0400

    Add functions for sorting a list and finding two numbers that add up to a target

--- arrays-lists/main.py
+++ arrays-lists/main.py
@@ -21,8 +21,36 @@ def common_elements(list1,list2):
             common.append(element)
     print(common)
 
-
+'''
+Sort a List of integers in highest to lowest
+'''
+def sort_list(list):
+    list.sort(reverse=True)
+    print(list)
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+'''
+def twoSum( nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result =[]
+        counter = 0
+        for num in nums:
+            pos = nums.index(target - num)
+            if(pos!=0):
+                result.append(counter)
+                result.append(pos)
+                break
+            counter +=1
+        return result
 
 
 # remove_duplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-common_elements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [ 6, 7, 16, 17, 18, 19, 20])
+# common_elements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [ 6, 7, 16, 17, 18, 19, 20])
+# sort_list([17,18,5,4,6,1])
+# print(twoSum([2, 7, 11, 15], 9)) # [0, 1]
+# print(twoSum([3,2,4], 6)) 
+print(twoSum([3,3], 6)) 
